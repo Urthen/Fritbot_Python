e1b24c7fcc30f2190869576f1e1f1c55d569b13d
Kyle Varga <kvarga@kvarga-ubuntu.(none)>
2012-01-24 22:00:41 -0600

    weather script

---
+++ fb/modules/weather.py
@@ -0,0 +1,31 @@
+import fb.intent as intent
+from fb.modules.base import FritbotModule, response
+
+# requirements
+import urllib, simplejson
+
+class WeatherModule(FritbotModule):
+
+        name="Weather"
+        description="Functions for returning weather"
+        author="Kyle Varga (kyle.varga@bazaarvoice.com)"
+
+	wundergroundapikey = 'http://api.wunderground.com/api/83f199a422e382c3/conditions/q/TX/Austin.json'
+	wundergroundapikey = '83f199a422e382c3'
+
+
+        def register(self):
+                intent.service.registerCommand("weather", self.getWeather, self, "Get Current Weather", "Gets the current weather.")
+                
+        @response
+        def getWeather(self, bot, room, user, args):
+		url = 'http://api.wunderground.com/api/' + self.wundergroundapikey + '/conditions/q/TX/Austin.json'
+		result = simplejson.load(urllib.urlopen(url))
+		current = result['current_observation']
+		return 'Current Weather in Austin, TX: ' + current['weather'] + ', ' + current['temperature_string']
+		#if 'Error' in result:
+        		# An error occurred; raise an exception
+        	#	return 'Error' +  result['Error']
+    		#return result['ResultSet']
+
+module = WeatherModule()
