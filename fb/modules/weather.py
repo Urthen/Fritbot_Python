53a3a825f1f316556e5de665fa0877406c10526b
Michael Pratt <urthen@gmail.com>
2012-01-25 09:53:33 -0600

    Merging weather module updates

--- fb/modules/weather.py
+++ fb/modules/weather.py
@@ -14,18 +14,22 @@ class WeatherModule(FritbotModule):
 
 	def register(self):
 		intent.service.registerCommand("weather", self.getWeather, self, "Get Current Weather", "Gets the current weather.")
-		
+			
 	@response
 	def getWeather(self, bot, room, user, args):
-		url = 'http://api.wunderground.com/api/' + self.wundergroundapikey + '/conditions/q/TX/Austin.json'
+		defaultLoc = 'TX/Austin' 
+		if len(args) > 0:
+			defaultLoc = urllib.quote_plus(' '.join(args))
+
+		url = 'http://api.wunderground.com/api/' + self.wundergroundapikey + '/conditions/q/' + defaultLoc + '.json'
 		f = urllib.urlopen(url)
 		buff = f.read().replace('\\/', '/')
+		f.close()
 		result = json.loads(buff)
-		current = result['current_observation']
-		return 'Current Weather in Austin, TX: ' + current['weather'] + ', ' + current['temperature_string']
-		#if 'Error' in result:
-			# An error occurred; raise an exception
-		#	return 'Error' +  result['Error']
-		#return result['ResultSet']
+		try:
+			current = result['current_observation']
+		except KeyError:
+			return 'No Weather Found for ' + defaultLoc
 
+		return 'Current Weather in ' + current['display_location']['full'] + ': ' + current['weather'] + ', ' + current['temperature_string']
 module = WeatherModule()
