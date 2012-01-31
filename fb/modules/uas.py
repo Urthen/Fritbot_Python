bb0ce18481db6873c24654ebc19e7a3e742b3196
Kyle Varga <kvarga@kvarga-ubuntu.(none)>
2012-01-31 09:03:10 -0600

    uas module

---
+++ fb/modules/uas.py
@@ -0,0 +1,20 @@
+import fb.intent as intent
+from fb.modules.base import FritbotModule, response
+
+class UASModule(FritbotModule):
+    name="UserAuthString"
+    description="Functions for user authentication strings"
+    author="Kyle Varga (kyle.varga@bazaarvoice.com)"
+
+    def register(self):
+        intent.service.registerCommand("decode", self.decodeUAS, self, "Decode Auth String", "Decodes Auth Strings.")
+        
+    @response
+    def decodeUAS(self, bot, room, user, args):
+        if len(args) > 0:
+            uas = args[0]
+        else:
+            return 'no auth string passed'
+        return uas[32:].decode('hex')
+    	
+module = UASModule()
