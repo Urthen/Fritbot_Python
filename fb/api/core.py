d582662305c5bb18630530bd99444770a1016c9f
Michael Pratt <urthen@gmail.com>
2012-03-26 00:49:33 -0500

    Enabling simple module APIs

--- fb/api/core.py
+++ fb/api/core.py
@@ -6,6 +6,8 @@ from twisted.application.internet import TCPServer
 from fb.api.quotes import HistoryList
 from fb.api.messaging import GroupChat
 from fb.api.auth import Retriever
+from fb.api.simple import SimpleModule
+from fb.api import util
 
 import config
 
@@ -19,32 +21,35 @@ class APIRoot(Resource):
 		self.putChild("groupmessage", GroupChat())
 		self.putChild("auth", Retriever())
 
-	def addModule(self, module):
-		self.putChild(module)
+	def addModule(self, name, module):
+		self.putChild(name, module)
 
 class APIWrapper(object):
 
 	def __init__(self):
 		self.root = None
-		self.preregistered = []
+		self.preregistered = {}
 
 	def launch(self, application):
 		if 'security' not in config.APPLICATION['modules']:
-			log("Warning: You have the API enabled, but not the 'security' module!")
+			log.msg("Warning: You have the API enabled, but not the 'security' module!")
 
 		# Initialize the API
 		self.root = APIRoot()
 
 		TCPServer(config.API['port'], Site(self.root)).setServiceParent(application)
 		for module in self.preregistered:
-			self.root.addModule(module)
+			self.root.addModule(module, self.preregistered[module])
 
 		self.preregistered = []
 
-	def registerModule(self, module):
+	def registerModule(self, name):
+		module = SimpleModule()
 		if self.root is None:
-			self.preregistered.append(module)
+			self.preregistered[name] = module
 		else:
-			self.root.addModule(module)
+			self.root.addModule(name, module)
+
+		return module
 	
 api = APIWrapper()
